6def12213e16fe0e8f1616489477f8df138778c2
GreikRodriguez <Greik.rodriguez@ucr.ac.cr>
2025-07-05 00:03:41 -0600

    respaldo main
    Main

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,9 @@ import App from './App'
 import HomePage from './components/pages/HomePage'
 import CreateEventPage from './components/pages/CreateEventPage'
 import AcordePage from './components/pages/AcordePage'
-<<<<<<< Updated upstream
-import NavBar from './components/ui/NavBar'
-=======
 import NavBar from './components/common/NavBar'
 import CategoriesPage from './components/pages/CategoriesPage'
 import EventPrivacy from './components/ui/EventPrivacy'
->>>>>>> Stashed changes
 import RegistroPage from './components/pages/RegistroPage'
 import PerfilPage from './components/pages/ProfilePage'
 import PerfilChats from './components/pages/PersonalChatsPage'
@@ -21,14 +17,11 @@ import OrgPage from './components/pages/OrganizerPage'
 import Notifications from './components/pages/NotificationsPage'
 
 
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-<<<<<<< Updated upstream
-    <AcordePage />
-=======
 
     <App />
 
->>>>>>> Stashed changes
   </StrictMode>,
 )
